299ee7f2724ee0b6fea65f8c7299c50b3174a081
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-05-04 14:05:11 -0500

    [IMP] doc

--- partner_wame/__manifest__.py
+++ partner_wame/__manifest__.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Partner wa.me',
+    "summary": "Enables a button to open wa.me based on the partners phone and mobile.",
     'version': '12.0.1.1.0',
     'author': 'HomebrewSoft',
-    'website': 'https://gitlab.com/HomebrewSoft/misc/partner_wame',
+    'website': 'https://github.com/HomebrewSoft/partner_wame',
+    'category': 'Human Resources',
+    'license': 'GPL-3',
     'depends': [
         'sms',
     ],
@@ -15,4 +18,8 @@
         'views/res_partner.xml',
     ],
     'auto_install': True,
+    'images': [
+        'static/description/images/main_screenshot.png',
+        'static/description/images/wame.png',
+    ],
 }
---
+++ partner_wame/doc/index.rst
@@ -0,0 +1,7 @@
+Partner wa.me
+=============
+
+Function:
+---------
+This module use the field `phone`  and `mobile` of `res.partner` model to display two buttons in order to open wa.me.
+Automatically use the field `phone_code` in `country_id` as prefix in the call.
